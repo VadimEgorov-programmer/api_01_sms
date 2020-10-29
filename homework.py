ce6f3c21f4f3105b74f9a0b16f682762a1448d4d
Vadim <wadim-egoroff@yandex.ru>
2020-10-29 16:24:26 +0300

    fix errors

--- homework.py
+++ homework.py
@@ -1,43 +1,45 @@
-import logging
 import time
+
 import requests
 from twilio.rest import Client
 from dotenv import load_dotenv
 import os
+import logging
+
 
 load_dotenv()
-VK_TOKEN = os.getenv('token')
-VK_API_V = '5.124'
-URL = 'https://api.vk.com/method/users.get'
-GET_USER = 'users.get'
-ACC_SID = os.getenv('account_sid')
-AUTH_TOKEN = os.getenv('auth_token')
+VK_API_VERSION = os.getenv('VK_API_VERSION')
+VK_TOKEN = os.getenv('VK_TOKEN')
+ACCOUNT_SID = os.getenv('ACCOUNT_SID ')
+AUTH_TOKEN = os.getenv('AUTH_TOKEN')
 NUMBER_FROM = os.getenv('NUMBER_FROM')
 NUMBER_TO = os.getenv('NUMBER_TO')
-client = Client(ACC_SID, AUTH_TOKEN)
+client = Client(ACCOUNT_SID, AUTH_TOKEN)
+URL = 'https://api.vk.com/method/'
+USER_GET = 'users.get'
 
 
 def get_status(user_id):
     params = {
         'user_ids': user_id,
         'fields': 'online',
-        'v': VK_API_V,
+        'v': VK_API_VERSION,
         'access_token': VK_TOKEN
     }
-    url = URL + GET_USER
+    url = URL + USER_GET
     try:
         request = requests.post(url, params=params).json()['response']
         user_status = request[0]['online']
         return user_status
     except Exception as e:
-        logging.exception(f"Error: {e}")
+        logging.exception(f'Request raised. Error: {e}')
 
 
 def sms_sender(sms_text):
     message = client.messages.create(
         body=sms_text,
         from_=NUMBER_FROM,
-        to=NUMBER_TO,
+        to=NUMBER_TO
     )
     return message.sid
 
